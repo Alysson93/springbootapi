2ae776ad665379b0eae3880548fd9ebba94934e6
Alysson Pereira | Grupo Moura <alysson.pereira@grupomoura.com>
2024-04-29 11:23:58 -0300

    exception handler

---
+++ src/main/java/com/example/springbootapi/rest/controllers/ApplicationControllerAdvice.java
@@ -0,0 +1,20 @@
+package com.example.springbootapi.rest.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.example.springbootapi.exceptions.RegraNegocioException;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+    
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException(RegraNegocioException exception) {
+        String message = exception.getMessage();
+        return new ApiErrors(message);
+    }
+
+}
